f7da99815fd4d19ff79c6321fd881d827a16a744
ludoloops <34949933+ludoloops@users.noreply.github.com>
2017-12-30 11:02:53 -0500

    Add files via upload
    coin list

---
+++ coin.py
@@ -0,0 +1,5 @@
+coin=("adzcoin", "auroracoin-qubit", "bitcoin", "bitcoin-cash", "bitcoin-gold", "dash", "digibyte-groestl",
+    "digibyte-qubit", "digibyte-skein", "electroneum", "ethereum", "ethereum-classic", "expanse", "feathercoin",
+    "gamecredits", "geocoin", "globalboosty", "groestlcoin", "litecoin","maxcoin", "monacoin","monero","musicoin",
+    "myriadcoin-groestl","myriadcoin-skein", "myriadcoin-yescrypt", "sexcoin", "siacoin", "startcoin", "verge-scrypt",
+    "vertcoin", "zcash", "zclassic", "zcoin", "zencash")
